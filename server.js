cf6fae9bd49b8f7fee5e3093ca08375a847dffa8
suvins <suvins8@gmail.com>
2025-07-13 20:28:40 +0400

    server.js update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 // CORS configuration for production
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://your-app-name.onrender.com'] 
+    ? ['https://portfolio-g2bs.onrender.com'] 
     : ['http://localhost:3000', 'http://127.0.0.1:5500'],
   credentials: true
 }));
